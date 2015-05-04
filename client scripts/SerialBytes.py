79b0bc7e799d92ebbefbb6e95a484fa3c9102498
DangerousElectrician <DangerousElectrician@gmail.com>
2015-05-04 16:04:57 -0500

    there's a semicolon in my python

--- client scripts/SerialBytes.py
+++ client scripts/SerialBytes.py
@@ -3,7 +3,7 @@ import serialComs
 def main():
 	com = serialComs.serialComs('COM6', 9600, timeout=4)
 	
-	com.write(b'b');
+	com.write(b'b')
 	com.writeInt32(-1000)
 	
 	#data = b"0aaa000aaaaaaaaa???"
