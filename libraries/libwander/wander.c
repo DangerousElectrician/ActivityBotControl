001f749aa9eefa3909934b6cda69fcb2710d584d
Fworg64 <fworg64@gmail.com>
2015-05-05 16:09:49 -0500

    Reupdated libwander
    Git still doesn't like me

--- libraries/libwander/wander.c
+++ libraries/libwander/wander.c
@@ -5,20 +5,21 @@
 
 static int *cog;
 
-void wander()
+int spin2find() //spins to find furthest distance
 {
-   int max_distance=0,index=0;
-   while(1)
-   {
+      high(26);
+      int max_distance =0;
+      int index =0;
+  
       for(int a=0; a<TurnK; a++)
       {
-        drive_speed(15,-15);              // rotate  360 deg
+        drive_speed(15,-15);              // rotate  like 10 deg
         pause(200);
       
         drive_speed(0,0);                  //stop
         pause(25);
       
-        int pos = getPingcm(); //update sensor values simultaneously enough.
+        int pos = getPingcm();
       
         if (pos > max_distance)            //initial scan
         {
@@ -27,8 +28,58 @@ void wander()
         }
         pause(25);                        //time to think
       }
+      low(26);
+      return index;
+}
+
+void go2index(int index)
+{
+      high(27);
+        for(int a=0; a<=index; a++)
+      {
+        drive_speed(15,-15);              // rotate  like 10 deg
+        pause(200);
+      
+        drive_speed(0,0);                  //stop
+        pause(50);
+      }
+      low(27);
+} 
+
+void charge2stop()
+{
+  high(26);
+  high(27);
+  drive_speed(20,20);
+  while (1)
+  {
+    if (getWhiskerL() ==0 || getWhiskerR() ==0)
+    {
+      drive_speed(-10,-10);
+      pause(1000);
+      low(26);
+      low(27);
+      return;
+    } 
+    
+    if (getPingcm() <= 5)
+    {
+      drive_speed(-10,-10);
+      pause(1000);
+      low(26);
+      low(27);
+      return;
+    }    
   }
-   
+}  
+
+void wander()
+{
+  while(1)
+  {
+    go2index(spin2find());
+    charge2stop();
+  }               
 }
 
 int *startWander()
