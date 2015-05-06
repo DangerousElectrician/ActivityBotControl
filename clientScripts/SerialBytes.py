072e8d28033914a39ad9573ac56b206f59dbcd82
DangerousElectrician <DangerousElectrician@gmail.com>
2015-05-06 16:35:43 -0500

    spaces are evil

---
+++ clientScripts/SerialBytes.py
@@ -0,0 +1,17 @@
+import serialComs
+
+def main():
+	com = serialComs.serialComs('COM6', 9600, timeout=4)
+	
+	com.write(b'b')
+	com.writeInt32(-1000)
+	
+	#data = b"0aaa000aaaaaaaaa???"
+	#com.write(data)
+	
+	while(True):
+		com.write(b'a')
+		number = com.readInt32()
+		print(number)
+		
+main()
---
+++ clientScripts/robotControl.py
@@ -0,0 +1,35 @@
+import serialComs
+
+class robotControl:
+
+	def __init__(self, port, baudrate, timeout):
+		self.com = serialComs.serialComs(port, baudrate, timeout)
+
+	def updateSensors(self):
+		self.com.write(b'0')		
+		self.ticks = [0,0]
+		self.whisker = [0,0]
+		self.com.write(b'v')
+		self.com.flushInput()
+		self.ticks[0] = self.com.readInt32()
+		self.ticks[1] = self.com.readInt32()
+		self.ping = self.com.readInt32()
+		self.whisker[0] = self.com.readInt32()
+		self.whisker[1] = self.com.readInt32()
+		
+	def getWhisker(self):
+		return self.whisker
+		
+	def getTicks(self):
+		return self.ticks
+		
+	def getPing(self):
+		return self.ping
+	
+	def getTheta(self):
+		return 0.03071833648393195*(self.ticks[1]-self.ticks[0]) # magic number
+		
+	def driveSpeed(self,l,r):
+		self.com.write(b'h')
+		self.com.writeInt32(l)
+		self.com.writeInt32(r)
