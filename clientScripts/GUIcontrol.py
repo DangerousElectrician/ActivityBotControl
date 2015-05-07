15873e43733cf597e762c4f06ae884fae79e9d85
DangerousElectrician <DangerousElectrician@gmail.com>
2015-05-06 23:06:31 -0500

    refactor

--- clientScripts/GUIcontrol.py
+++ clientScripts/GUIcontrol.py
@@ -8,34 +8,31 @@ from pylab import *
 top = tkinter.Tk()
 rob = robotControl.robotControl(sys.argv[1], 9600, timeout=4)
 
-ticks = [0,0]
-whisker = [0,0]
-ping = 0
+def init():
+	rob.startSensors()
+	rob.startDrive()
+	
+def off():
+	rob.stopSensors()
+	rob.stopDrive()
+
+	
+def startPlot():
+	plotData(rob.getTheta())
 
 def readSensors():
 	rob.updateSensors()
-	ticks = rob.getTicks()
-	ping = rob.getPing()
-	whisker = rob.getWhisker()
-	#print("p "+str(ping)+"\ttl "+str(ticks[0])+"\ttr "+str(ticks[1])+"\twl "+str(whisker[0])+"\twr "+str(whisker[1]))
-	#print(str(math.degrees(rob.getTheta())))
 	top.after(1,readSensors)
-
-def startMotor():
-	rob.driveSpeed(-5,5)
-
-def stopMotor():
-	rob.driveSpeed(0,0)
 	
 ra= []
 th = []
-def printData():
-	startMotor()
+def plotData(initth):
+	rob.driveSpeed(-5,5)
 	print(str(math.degrees(rob.getTheta()))+"\t"+str(rob.getPing()))
 	ra.append(rob.getPing())
 	th.append(rob.getTheta())
-	if(rob.getTheta()>2*math.pi):
-		stopMotor()
+	if((rob.getTheta()-initth)>2*math.pi):
+		rob.driveSpeed(0,0)
 		print(ra,th)
 		
 		r= np.array(ra)
@@ -45,16 +42,19 @@ def printData():
 		c = scatter(theta, r)
 		plt.show()
 	else:
-		top.after(1,printData)
+		top.after(1,lambda:plotData(initth))
 	
+def main():
+	B = tkinter.Button(top, text ="init", command = init)
+	B3 = tkinter.Button(top, text ="scan", command = startPlot)
+	B2 = tkinter.Button(top, text ="off", command = off)
+	Breset = tkinter.Button(top, text ="reset", command = rob.reset)
+
+	B.pack()
+	B2.pack()
+	B3.pack()
+	Breset.pack()
+	top.after(1,readSensors)
+	top.mainloop()
 	
-B = tkinter.Button(top, text ="spin", command = startMotor)
-B2 = tkinter.Button(top, text ="stop", command = stopMotor)
-B3 = tkinter.Button(top, text ="scan", command = printData)
-
-
-B.pack()
-B2.pack()
-B3.pack()
-top.after(1,readSensors)
-top.mainloop()
+main()
