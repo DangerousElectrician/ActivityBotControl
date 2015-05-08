2bd9ce4cc2688cb42927eaf3d1462a36857d98d9
DangerousElectrician <DangerousElectrician@gmail.com>
2015-05-07 20:54:48 -0500

    adding points to graph test

--- clientScripts/testPlotting.py
+++ clientScripts/testPlotting.py
@@ -7,15 +7,15 @@ ion()
 tstart = time.time()               # for profiling
 h, = plot([],[])
 
-def update_line(hl, new_data):
-	hl.set_xdata(numpy.append(hl.get_xdata(), new_data))
-	hl.set_ydata(numpy.append(hl.get_ydata(), new_data))
+def update_line(hl, new_datax, new_datay):
+	hl.set_xdata(numpy.append(hl.get_xdata(), new_datax))
+	hl.set_ydata(numpy.append(hl.get_ydata(), new_datay))
 	gca().relim()
 	gca().autoscale_view()
 	draw()
 
 for i in arange(1,200):
 	print(i)
-	update_line(line,i)#.set_ydata(sin(x+i/10.0))  # update the data
+	update_line(h,i,2*i)#.set_ydata(sin(x+i/10.0))  # update the data
 		
 print( 'FPS:' , 200/(time.time()-tstart))
